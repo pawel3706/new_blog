fd2cd36e419653bb6853e415659b95b1bc32748b
Paweł Nowiński <pawel3706@gmail.com>
2019-08-09 10:37:52 +0200

    Add class active to hero section

--- js/init.js
+++ js/init.js
@@ -3,8 +3,8 @@ class Init {
 
         this.navList = new NavList();
 
-
         this.navList.button.addEventListener('click', this.navList.toggleNav.bind(this.navList));
+        
         document.querySelectorAll('a').forEach(link => link.addEventListener('click', e => e.preventDefault()));
     }
 }
